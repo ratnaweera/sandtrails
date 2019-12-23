8479b089f5abc2093658474698389fbe6924f254
Roman Ratnaweera <roman@ratnaweera.net>
2019-12-23 19:08:28 +0100

    Rho axis works im mm. Homing not yet possible, no sensor

--- sandtrails.py
+++ sandtrails.py
@@ -1,6 +1,6 @@
 from time import sleep
+from math import pi
 import RPi.GPIO as GPIO
-import curses
 
 TH_DIR = 5   # Direction GPIO Pin
 TH_STEP = 6  # Step GPIO Pin
@@ -10,80 +10,118 @@ RH_DIR = 21   # Direction GPIO Pin
 RH_STEP = 20  # Step GPIO Pin
 RH_MODE = (22, 27, 17)   # Microstep Resolution GPIO Pins
 
-CW = 1     # Clockwise Rotation
-CCW = 0    # Counterclockwise Rotation
-SPR = 200   # Steps per Revolution
+CW = 1      # Clockwise Rotation
+CCW = 0     # Counterclockwise Rotation
+SPR = 200*32   # Steps per Revolution
+DRHO = 14   # Diameter spur gear of Rho axis [mm]
+AXISTYPE = {
+    'Rho': 0,
+    'Theta': 1}
+delay = 0.006 / 32
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(TH_DIR, GPIO.OUT)
-GPIO.setup(TH_STEP, GPIO.OUT)
-GPIO.setup(RH_DIR, GPIO.OUT)
-GPIO.setup(RH_STEP, GPIO.OUT)
-GPIO.output(TH_DIR, CW)
-GPIO.output(RH_DIR, CW)
+def setup_steppermotors():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(TH_DIR, GPIO.OUT)
+    GPIO.setup(TH_STEP, GPIO.OUT)
+    GPIO.setup(RH_DIR, GPIO.OUT)
+    GPIO.setup(RH_STEP, GPIO.OUT)
+    GPIO.output(TH_DIR, CW)
+    GPIO.output(RH_DIR, CW)
 
-GPIO.setup(TH_MODE, GPIO.OUT)
-GPIO.setup(RH_MODE, GPIO.OUT)
-RESOLUTION = {'Full': (0, 0, 0),
-              'Half': (1, 0, 0),
-              '1/4': (0, 1, 0),
-              '1/8': (1, 1, 0),
-              '1/16': (0, 0, 1),
-              '1/32': (1, 0, 1)}
-GPIO.output(RH_MODE, RESOLUTION['1/32'])
-GPIO.output(TH_MODE, RESOLUTION['1/32'])
+    GPIO.setup(TH_MODE, GPIO.OUT)
+    GPIO.setup(RH_MODE, GPIO.OUT)
+    RESOLUTION = {'Full': (0, 0, 0),
+                  'Half': (1, 0, 0),
+                  '1/4': (0, 1, 0),
+                  '1/8': (1, 1, 0),
+                  '1/16': (0, 0, 1),
+                  '1/32': (1, 0, 1)}
+    GPIO.output(RH_MODE, RESOLUTION['1/32'])
+    GPIO.output(TH_MODE, RESOLUTION['1/32'])
 
-#step_count = SPR * 32
-step_count = SPR / 10
-delay = .002 / 32
-
-# Set up curses
-screen = curses.initscr() # get the curses screen window
-curses.noecho() # turn off input echoing
-curses.cbreak() # respond to keys immediately (don't wait for enter)
-screen.keypad(True) # map arrow keys to special values
-
-
-
+    
+class axis:
+    def __init__(self, axistype, llimit, ulimit):
+        self.axistype = axistype
+        self.llimit = llimit
+        self.ulimit = ulimit
+        self.curSteps = 0
+        self.curPos = 0
+        self.tarSteps = 0
+        self.tarPos = 0
+        self.homed = False
+    
+    def printState(self):
+        print("(" + str(self.curSteps) + ", " + str(self.curPos) + ", " + str(self.tarSteps) + ", " + str(self.tarPos) + ") ( curSteps, curPos, tarSteps, tarPos)")
+        
+    def homing(self):
+        # Drive until we see the sensor, depends on type
+        self.curSteps = self.curPos = 0
+        self.homed = True
+        
+    def goTo(self, dest):
+        if not self.homed:
+            print("Not homed!")
+            return -1
+        elif (dest < self.llimit) or (dest > self.ulimit):
+            print("target violates limits")
+            return -1
+        else:
+            self.tarPos = dest
+            if self.tarPos == self.curPos:
+                print("Already at target position " + str(self.tarPos))
+                return 0
+            else:
+                print("target: " + str(self.tarPos) + " [mm]")
+                if self.axistype == AXISTYPE['Rho']:
+                    self.tarSteps = round(self.tarPos * SPR / (pi*DRHO))
+                    print("target: " + str(self.tarSteps) + " [steps]")
+                    for x in range(abs(self.tarSteps - self.curSteps)):
+                        if self.curSteps < self.tarSteps:
+                            GPIO.output(RH_DIR, CW)
+                            GPIO.output(RH_STEP, GPIO.HIGH)
+                            sleep(delay)
+                            GPIO.output(RH_STEP, GPIO.LOW)
+                            sleep(delay)
+                            self.curSteps += 1
+                        else:
+                            GPIO.output(RH_DIR, CCW)
+                            GPIO.output(RH_STEP, GPIO.HIGH)
+                            sleep(delay)
+                            GPIO.output(RH_STEP, GPIO.LOW)
+                            sleep(delay)
+                            self.curSteps -= 1
+                        self.curPos = self.curSteps * pi * DRHO / SPR
+                    self.printState()    
+                return 0
+        
+    
 try:
-    screen.addstr(0, 0, 'Press q to quit')
-    while True:
-        char = screen.getch()
-        if char == ord('q'):
-            break
-        elif char == curses.KEY_RIGHT:
-            for x in range(step_count):
-                GPIO.output(TH_DIR, CW)
-                GPIO.output(TH_STEP, GPIO.HIGH)
-                sleep(delay)
-                GPIO.output(TH_STEP, GPIO.LOW)
-                sleep(delay)
-        elif char == curses.KEY_LEFT:
-            for x in range(step_count):
-                GPIO.output(TH_DIR, CCW)
-                GPIO.output(TH_STEP, GPIO.HIGH)
-                sleep(delay)
-                GPIO.output(TH_STEP, GPIO.LOW)
-                sleep(delay)       
-        elif char == curses.KEY_UP:
-            for x in range(step_count):
-                GPIO.output(RH_DIR, CW)
-                GPIO.output(RH_STEP, GPIO.HIGH)
-                sleep(delay)
-                GPIO.output(RH_STEP, GPIO.LOW)
-                sleep(delay)              
-        elif char == curses.KEY_DOWN:
-            for x in range(step_count):
-                GPIO.output(RH_DIR, CCW)
-                GPIO.output(RH_STEP, GPIO.HIGH)
-                sleep(delay)
-                GPIO.output(RH_STEP, GPIO.LOW)
-                sleep(delay)
-        
-        
+    setup_steppermotors()
+    
+    rho = axis(AXISTYPE['Rho'], -5, 100)
+    rho.printState()
+    rho.homing()
+    
+    rho.goTo(10)
+    rho.printState()
+    sleep(5)
+    
+    rho.goTo(20)
+    rho.printState()
+    sleep(5)
+    
+    rho.goTo(80)
+    rho.printState()
+    sleep(5)
+    
+    rho.goTo(0)
+    rho.printState()
+    sleep(5)
+    print("Main loop done")
+    
 finally:
     # shut down cleanly
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
     GPIO.cleanup()
+    print("Exited normally")
 
